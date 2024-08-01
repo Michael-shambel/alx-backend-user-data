5710026c068543000a456f1604f6b2a2cbace2d2
michael-shambel <Michaelshambel10@gmail.com>
2024-08-01 14:23:59 +0300

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,7 +11,7 @@ PII_FIELDS = ('email', 'password', 'ssn', 'phone', 'address')
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    pattern = '|'.join(f'{field}=[^;]*' for field in fields)
+    pattern = f"({'|'.join(fields)})=[^{separator}]*"
     sub_str = re.sub(pattern, lambda m:
                      f"{m.group().split('=')[0]}={redaction}", message)
     return sub_str
