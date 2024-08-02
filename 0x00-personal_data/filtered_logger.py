ff241f60ce51c9789376c22679e65b66456cff94
michael-shambel <Michaelshambel10@gmail.com>
2024-08-02 17:17:19 +0300

    task all

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,6 +13,9 @@ PII_FIELDS = ('email', 'password', 'ssn', 'phone', 'address')
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
+    """
+    returns the log message obfuscated
+    """
     pattern = f"({'|'.join(fields)})=[^{separator}]*"
     sub_str = re.sub(pattern, lambda m:
                      f"{m.group().split('=')[0]}={redaction}", message)
@@ -20,23 +23,33 @@ def filter_datum(fields: List[str], redaction: str,
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """ 
+    Redacting Formatter class
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """
+        constructor
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """
+        format method
+        """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
 
 
 def get_logger() -> logging.Logger:
+    """
+    returns a logging object
+    """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
@@ -47,6 +60,9 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    returns a connector to the database
+    """
     username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
     password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
     host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
