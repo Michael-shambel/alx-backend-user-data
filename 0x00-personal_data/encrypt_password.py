7dcfcf88169a9c7dcba633838af4e4463d86c84b
michael-shambel <Michaelshambel10@gmail.com>
2024-08-04 01:26:49 +0300

    asasaas

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module for password hashing.
+"""
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a password using bcrypt.
+    
+    Args:
+        password (str): The password to hash.
+        
+    Returns:
+        bytes: The salted, hashed password.
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
