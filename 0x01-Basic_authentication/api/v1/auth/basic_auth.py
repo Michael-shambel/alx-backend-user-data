645b34d69444a4c33751e64bc9d63f9e3eb61efe
michael-shambel <Michaelshambel10@gmail.com>
2024-08-06 16:42:13 +0300

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Basic auth
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -13,7 +14,7 @@ class BasicAuth(Auth):
                                             authorization_header:
                                             str) -> str:
         """
-        decoding base64 autorization header
+        extracting base64 autorization header from the type of authontication
         """
         if authorization_header is None:
             return None
@@ -22,3 +23,19 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+        decoding the authorization header to the formal input
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
