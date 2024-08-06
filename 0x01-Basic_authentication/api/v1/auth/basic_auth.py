99c65e6d8a862d319cb0c36296f0e2de138ec469
michael-shambel <Michaelshambel10@gmail.com>
2024-08-06 18:08:02 +0300

    task assss

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -75,3 +75,34 @@ class BasicAuth(Auth):
         if not user.is_valid_password(user_pwd):
             return None
         return user
+
+
+def current_user(self, request=None) -> TypeVar('User'):
+    """
+    overloads Auth and retrieves the User instance for a request
+    """
+    if request is None:
+        return None
+    authorization_header = self.authorization_header(request)
+    if authorization_header is None:
+        return None
+
+    base64_authorization_header = (
+        self.extract_base64_authorization_header(authorization_header)
+        )
+    if base64_authorization_header is None:
+        return None
+
+    decoded_base64_authorization_header = (
+        self.decode_base64_authorization_header(base64_authorization_header)
+        )
+    if decoded_base64_authorization_header is None:
+        return None
+
+    user_email, user_pwd = self.extract_user_credentials(
+        decoded_base64_authorization_header)
+    if user_email is None or user_pwd is None:
+        return None
+
+    user = self.user_object_from_credentials(user_email, user_pwd)
+    return user
