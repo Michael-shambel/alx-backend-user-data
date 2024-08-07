d2f0370deceb1a067b48dd3c0837fb25dd7645c9
michael-shambel <Michaelshambel10@gmail.com>
2024-08-07 15:49:11 +0300

    task back1222

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,7 +18,7 @@ auth_type = getenv('AUTH_TYPE')
 if auth_type == 'BasicAuth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif auth_type == 'auth':
+else:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
