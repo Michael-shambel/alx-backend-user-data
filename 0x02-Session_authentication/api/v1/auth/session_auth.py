9d441ffbd776fad753f760eb9a7958caf130313e
michael-shambel <Michaelshambel10@gmail.com>
2024-08-10 12:48:54 +0300

    task 1 qw

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """
 SessionAuth module for the API
 """
+import uuid
 from api.v1.auth.auth import Auth
 
 
@@ -9,4 +10,16 @@ class SessionAuth(Auth):
     """
     SessionAuth class for session-based authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a Session ID for a user_id and stores it in the dictionary
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
