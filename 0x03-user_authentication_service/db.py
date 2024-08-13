3fbcdc9ba7c5af042ca963fc2c2f997c753080d6
michael-shambel <Michaelshambel10@gmail.com>
2024-08-13 12:27:53 +0300

    task 1 after downgrade version of sqlalchemy

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,11 +6,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import User
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
-from user import Base
+from user import Base, User
 
 
 class DB:
