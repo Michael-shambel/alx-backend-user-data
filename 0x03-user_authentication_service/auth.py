5c8d1187dc06b686bd9ca0c8c19f03b0df052756
michael-shambel <Michaelshambel10@gmail.com>
2024-08-13 14:59:12 +0300

    task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -64,3 +64,16 @@ class Auth:
             return bcrypt.checkpw(input_password, user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """
+        Create a new session for a user.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            user.session_id = session_id
+            self._db._session.commit()
+            return session_id
+        except NoResultFound:
+            return None
