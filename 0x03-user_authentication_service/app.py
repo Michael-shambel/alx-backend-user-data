042dcb29f7fd84bc13ef9615037ae881387e4ab7
michael-shambel <Michaelshambel10@gmail.com>
2024-08-14 16:20:40 +0300

    task 16

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -87,5 +87,20 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'])
+def reset_password():
+    """
+    handle POST request asked by the user
+    the code request email from the form
+    and generate a reset token for the user
+    """
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
