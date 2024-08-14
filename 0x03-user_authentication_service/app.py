6613a39d61043e49fee45566d5044e52e0051e94
michael-shambel <Michaelshambel10@gmail.com>
2024-08-14 16:40:56 +0300

    task 15

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -84,7 +84,7 @@ def profile():
     user = AUTH.get_user_by_session_id(session_id)
     if user is None:
         abort(403)
-    return jsonify({"email": user.email}), 200
+    return jsonify({"email": user.email})
 
 
 @app.route('/reset_password', methods=['POST'])
