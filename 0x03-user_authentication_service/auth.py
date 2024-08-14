6d4c5929ddf3f2f4079c33320a50dd8c10d4cbb3
michael-shambel <Michaelshambel10@gmail.com>
2024-08-14 22:21:58 +0300

    task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -55,12 +55,13 @@ class Auth:
             if it is in the db error
         """
         try:
-            self._db.find_user_by(email=email)
-            raise ValueError(f"User {email} already exists")
-
+            existing_user = self._db.find_user_by(email=email)
+            if existing_user:
+                raise ValueError(f"User {email} already exists")
         except NoResultFound:
             hashed_password = _hash_password(password)
-            user = self._db.add_user(email, hashed_password)
+            user = self._db.add_user(email=email,
+                                     hashed_password=hashed_password)
             return user
 
     def valid_login(self, email: str, password: str) -> bool:
