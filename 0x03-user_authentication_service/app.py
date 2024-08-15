5fdc0b9776f52cab6225098679746988b11a74bd
michael-shambel <Michaelshambel10@gmail.com>
2024-08-15 10:48:00 +0300

    task 15 typing error

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -71,15 +71,17 @@ def logout():
         abort(403)
 
 
-@app.route('/profile', methods=['GET'], strict_slashes=False)
-def profile() -> str:
+@app.route('/profile', methods=['GET'])
+def profile():
     """
     handle GET request asked by the user
     the code request session id from the cookie
     and return  the email of the user
     """
     session_id = request.cookies.get('session_id')
-    user = AUTH.get_user_by_session_id(session_id)
+    if not session_id:
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
     return jsonify({"email": user.email}), 200
